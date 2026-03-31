d76335c6d0991f7d3b543ec3e8b732bd0900e65f
Amadou Barrie <amadoubarrie@Mac.lan>
2026-03-31 08:32:34 -0400

    final commit

--- src/analysis.py
+++ src/analysis.py
@@ -6,26 +6,28 @@ import seaborn as sns
 
 data = pd.read_csv('/Users/amadoubarrie/Desktop/job-market-pipeline/data/cleaned/cleaned_jobs.csv')
 
-results = data.groupby('Organization')['max-salary'].mean().sort_values(ascending = False).head(10)
+data.to_csv("final_jobs.csv", index=False)
+
+results = data.groupby('Organization')['max-salary'].mean().sort_values(ascending = False)
 
 
-location_results =data['Location'].value_counts().sort_values(ascending= False).head(10)
+location_results =data['Location'].value_counts().sort_values(ascending= False)
 print(location_results)
 
 
 sns.barplot(x=results.values, y=results.index)
-plt.title("Top 10 Highest Paying Organizations")
+plt.title("Top Highest Paying Organizations")
 plt.xlabel("Average Max Salaries")
 plt.ylabel("Organizations")
 plt.tight_layout()
 
 sns.barplot(x=location_results.values, y=location_results.index)
-plt.title("Top 10 Locations of the highest paying Organizations")
+plt.title("Top Locations of the highest paying Organizations")
 plt.xlabel("Location count")
 plt.ylabel("Organizations")
 plt.tight_layout()
 plt.show()
 
 
-sns.histplot(data=data, x="max-salary")
-plt.show()
+# sns.histplot(data=data, x="max-salary")
+# plt.show()
